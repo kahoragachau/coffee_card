7e6abb58123d0ef41349dd943f9dfaafdfb99add
kahoragachau <joelkahoragachau6@gmail.com>
2024-03-13 23:02:21 +0300

    feat: create home component

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,9 @@
+import 'package:coffee_card/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
     home: Home()
   ));
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Coffee Id'),
-        backgroundColor: Colors.brown[700],
-        centerTitle: true,
-      ),
-      body: const Home(),
-    );
-  }
 }
 
