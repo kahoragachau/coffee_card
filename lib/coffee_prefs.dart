402831309b08cedcf7384f6dd51a4af1348f3f77
kahoragachau <joelkahoragachau6@gmail.com>
2024-03-16 23:19:39 +0300

    feat: Add Rows to coffee prefs component

--- lib/coffee_prefs.dart
+++ lib/coffee_prefs.dart
@@ -5,6 +5,23 @@ class CoffeePrefs extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Column(
+      children: [
+        Row(
+          children: [
+            Text('Strength: '),
+            Text('3'),
+            SizedBox(width: 50),
+          ],
+        ),
+                Row(
+          children: [
+            Text('Sugars: '),
+            Text('3'),
+            SizedBox(width: 50),
+          ],
+        )
+      ],
+    );
   }
 }
